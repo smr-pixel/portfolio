eeeffa74d430ba2443568ffd3b04ca4187e690d0
smr-pixel <smr239@pitt.edu>
2025-08-16 13:07:46 -0400

    create home button and prepare for navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,12 +8,12 @@ const Home: React.FC = () => {
     return (
         <main className="home" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <section className="hero">
-                <img src={logo} alt="Logo" className="Logo" />
+                <a href="portfolio/home"> <img src={logo} alt="Logo" className="Logo" /></a>
                 <h1>Hi, I'm Sarah Reyer, <br />An Aspiring Cybersecurity Professional</h1>
                 <p className="intro">
                     Information Science, Computer Science, and Global Policy student at the Unversity of Pittsburgh
                     <br />
-                    Founding Vice President of <a href="https://www.linkedin.com/company/wicys-pitt/"> WiCyS (Women in CyberSecurity) at Pitt </a>
+                    Founding Vice President of <a href="https://www.linkedin.com/company/wicys-pitt/"> WiCyS (Women in CyberSecurity) @ Pitt </a>
                 </p>
                 <img src={profile} alt="Profile" className="profile-image" />
 
