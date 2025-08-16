f025fcbb8a6f0a95c3395074b293639a188c914d
smr-pixel <smr239@pitt.edu>
2025-08-16 01:07:29 -0400

    develop home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import './Home.css';
 
 const Home: React.FC = () => {
     return (
         <main className="home">
             <section className="hero">
-                <h1>Hi, I'm Sarah Reyer</h1>
+                <img src="public/logo-2-removebg-preview.png" alt="Logo" className="Logo" />
+                <h1>Hi, I'm Sarah Reyer, <br />An Aspiring Cybersecurity Professional</h1>
                 <p className="intro">
-                    Welcome to my portfolio! I am a cybersecurity professional with a focus on 
-                    protecting individuals and organizations through technical solutions and sound policy. 
-                    Here you can find more about my work, projects, and contributions to the field.
+                    Information Science, Computer Science, and Global Policy student at the Unversity of Pittsburgh
+                    <br />
+                    Founding Vice President of <a href="https://www.linkedin.com/company/wicys-pitt/"> WiCyS (Women in CyberSecurity) at Pitt </a>
                 </p>
+                <img src="public/profile.jpeg" alt="Profile" className="profile-image" />
+
             </section>
         </main>
     );
