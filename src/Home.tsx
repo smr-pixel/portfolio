18fac343eb28fcae3db18f6c25a9dbce8cd6f5c8
smr-pixel <smr239@pitt.edu>
2025-08-16 13:11:44 -0400

    fix home link

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     return (
         <main className="home" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <section className="hero">
-                <a href="/"> <img src={logo} alt="Logo" className="Logo" /></a>
+                <a href="/portfolio"> <img src={logo} alt="Logo" className="Logo" /></a>
                 <h1>Hi, I'm Sarah Reyer, <br />An Aspiring Cybersecurity Professional</h1>
                 <p className="intro">
                     Information Science, Computer Science, and Global Policy student at the Unversity of Pittsburgh
