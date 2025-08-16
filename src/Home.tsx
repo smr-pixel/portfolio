29197542b74319ba2dd234de0e7f91756aefdced
smr-pixel <smr239@pitt.edu>
2025-08-16 01:10:58 -0400

    remove public from images to see if works with react

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,14 +5,14 @@ const Home: React.FC = () => {
     return (
         <main className="home">
             <section className="hero">
-                <img src="public/logo-2-removebg-preview.png" alt="Logo" className="Logo" />
+                <img src="/logo-2-removebg-preview.png" alt="Logo" className="Logo" />
                 <h1>Hi, I'm Sarah Reyer, <br />An Aspiring Cybersecurity Professional</h1>
                 <p className="intro">
                     Information Science, Computer Science, and Global Policy student at the Unversity of Pittsburgh
                     <br />
                     Founding Vice President of <a href="https://www.linkedin.com/company/wicys-pitt/"> WiCyS (Women in CyberSecurity) at Pitt </a>
                 </p>
-                <img src="public/profile.jpeg" alt="Profile" className="profile-image" />
+                <img src="/profile.jpeg" alt="Profile" className="profile-image" />
 
             </section>
         </main>
